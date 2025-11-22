f9b8958ca5366e35819f54fcf724a7c0525b348f
Engyhesh <137059464+Engyhesh@users.noreply.github.com>
2025-11-22 12:56:03 +0200

    update

--- src/pages/Snacks/Snacks.jsx
+++ src/pages/Snacks/Snacks.jsx
@@ -45,7 +45,7 @@ const Snacks = () => {
         name: "Classic Popcorn",
         description: "Freshly popped butter popcorn",
         price: 8,
-        image: "../../../public/movie-theatre-popcorn.png",
+        image: "../../../public/movie-theatre-popcorn.jpg",
         item: 0,
         },
         {
